6cbfdba708be02aa29ed03bfa83032b86a76e353
Raishawn Jones <141539039+LLTS1022@users.noreply.github.com>
2025-02-16 17:54:38 -0600

    2nd Update RecursiveProduct.java

--- Module_5/RecursiveProduct.java
+++ Module_5/RecursiveProduct.java
@@ -6,10 +6,10 @@ public class RecursiveProduct {
         //Base case: If the index reaches 0, return the first element of the array.
         if (index == 0) {
             return numbers[0];
-        }
+        };
         //Recursive case: Multiply the current element with the reuive call.
         return numbers[index] * product(numbers, index - 1);
-    } 
+    }; 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,5 +17,16 @@ public class RecursiveProduct {
 
         //Getting the user's input
         System.out.println("Hello, enter 5 numbers: ");
-    }
-}
+        for (int i = 0; i < 5; i++) {
+            numbers[i] = scanner.nextInt();
+    };
+
+    //Calculate the product using a recursive method
+    int result = product(numbers, numbers.length - 1);
+
+    //Display the result in the console.
+    System.out.println("The product of the numbers is: " + result);
+    
+    scanner.close(); //
+    };
+};
