ccf22f20566567e0a270fb29d117ae1855216e3b
Raishawn Jones <141539039+LLTS1022@users.noreply.github.com>
2025-02-23 21:57:02 -0600

    first commit, setting up initial program structure

---
+++ Module_6/Module-6-CTA/Student.java
@@ -0,0 +1,2 @@
+//This file defines the Student class and contains the "main" method, which creates an arraylist of 10 students, prints them unsorted, sorts them first by name, then by roll #, and then prints out the sorted list.
+
