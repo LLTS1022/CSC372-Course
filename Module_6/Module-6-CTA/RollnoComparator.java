4335217ffe446da6af233b9c3d8ebf9fb20b8e4e
Raishawn Jones <141539039+LLTS1022@users.noreply.github.com>
2025-02-24 20:03:02 -0600

    Update RollnoComparator.java

--- Module_6/Module-6-CTA/RollnoComparator.java
+++ Module_6/Module-6-CTA/RollnoComparator.java
@@ -1,3 +1,8 @@
-public class RollnoComparator {
-    
+import java.util.Comparator;
+
+public class RollnoComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        return s1.rollno - s2.rollno;
+    }
 }
